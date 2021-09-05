5b2542888daff939c5eba24194deaa1f78bc90ad
programming-lab-Reshma <76769343+reshma-balu@users.noreply.github.com>
2021-09-05 22:23:35 +0530

    Add files via upload

---
+++ CO3PG4.java
@@ -0,0 +1,20 @@
+import java.awt.*;  
+import java.applet.*;  
+ 
+public class Shapes extends Applet
+{
+ public void paint(Graphics g)
+ {
+  g.setFont(new Font("Cambria", Font.BOLD,15));
+  g.drawString("Different Shapes", 15, 15);
+  g.drawLine(10,20,50,60);
+  g.drawRect(10,70,40,40);
+  g.setColor(Color.RED);
+  g.fillOval(60,20,30,90);
+  g.fillArc(60,135,80,40,180,180);
+  g.fillRoundRect(20,120,60,30,5,5);
+ }
+}
+/* <applet code="Shapes" width=200 height=200>
+ </applet>
+*/
